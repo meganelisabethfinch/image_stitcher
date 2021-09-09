dd08ee60b033e176bee45b7ea5cad501592aeaf2
meganelisabethfinch <meganelisabethfinch@gmail.com>
2021-09-09 21:54:48 +0100

    stitch two images

--- src/image-stitching.cpp
+++ src/image-stitching.cpp
@@ -3,7 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/features2d.hpp>
-#include <opencv2/gapi/own/types.hpp>
+#include <opencv2/core/types.hpp>
 
 using namespace cv;
 
@@ -11,28 +11,22 @@ int main(int argc, char** argv )
 {
     if ( argc != 3 )
     {
-        printf("usage: ./image-stitching <Img1_Path> <Img2_Path> \n");
+        printf("usage: ./image-stitching <Img1Path> <Img2Path> \n");
         return -1;
     }
-    cv::Mat img1;
-    img1 = imread(argv[1], 0);
-    if ( !img1.data )
-    {
-        printf("No image data for image 1 \n");
-        return -1;
-    }
-
-    cv::Mat img2;
-    img2 = imread(argv[2], 0);
-    if ( !img2.data )
-    {
-        printf("No image data for image 2 \n");
-        return -1;
-    }
+        
     std::vector<cv::Mat> images;
-    images.push_back(img1);
-    images.push_back(img2);
-   
+
+    // IMPORTANT: Flipped the order of images, the array goes right to left
+    for (int i = 2; i > 0; i--) {
+        cv::Mat img = imread(argv[i], 0);
+        if (!img.data) {
+            printf("No image data.\n");
+            return -1;
+        }
+        images.push_back(img);
+    }
+    
     // Set-up SIFT
     cv::Ptr<cv::SIFT> detector = cv::SIFT::create();
 
@@ -55,49 +49,70 @@ int main(int argc, char** argv )
     std::vector<std::vector<cv::DMatch>> rawMatches;
     matcher->knnMatch(allDescriptors[0], allDescriptors[1], rawMatches, 2);
 
-   std::vector<cv::DMatch> goodMatches;
+    std::vector<cv::DMatch> goodMatches;
 
-   // Collect keypoints and coords for good matches
-   std::vector<KeyPoint> kps1;
-   std::vector<KeyPoint> kps2;
-   std::vector<Point2f> pts1;
-   std::vector<Point2f> pts2; 
+    for (int i = 0; i < rawMatches.size(); i++) {
+        std::vector<cv::DMatch> m = rawMatches[i];
+        if (m.size() == 2 && (m[0].distance < m[1].distance * 0.6)) {
+            // Lowe's Ratio Test: If d1/d2 < 0.6, this is a good match
+            goodMatches.push_back(m[0]);
+        }
+    }
 
-   for (int i = 0; i < rawMatches.size(); i++) {
-       std::vector<cv::DMatch> m = rawMatches[i];
-       if (m.size() == 2 && (m[0].distance < m[1].distance * 0.6)) {
-           // Lowe's Ratio Test: If d1/d2 < 0.6, this is a good match
-           goodMatches.push_back(m[0]);
-           kps1.push_back(allKeypoints[0][m[0].queryIdx]);
-           kps2.push_back(allKeypoints[1][m[0].trainIdx]);
-           pts1.push_back(allKeypoints[0][m[0].queryIdx].pt);
-           pts2.push_back(allKeypoints[1][m[0].trainIdx].pt);
-       }
-   }
+    // Collect keypoints coords for good matches
+    std::vector<cv::Point2f> pts1;
+    std::vector<cv::Point2f> pts2; 
 
-   // check we have enough matches for homography
-   if (goodMatches.size() < 4) {
+    for (int i = 0; i < goodMatches.size(); i++) {
+        int idx = goodMatches[i].queryIdx;
+        pts1.push_back(allKeypoints[0][idx].pt); // ** I think this bit is wrong :-(
+    }
+
+    for (int i = 0; i < goodMatches.size(); i++) {
+        int idx = goodMatches[i].trainIdx;
+        pts2.push_back(allKeypoints[1][idx].pt);
+    }
+
+    // check we have enough matches for homography
+    if (goodMatches.size() < 4) {
        printf("Not enough matches. 4+ matches are needed to compute a homography.\n");
        return -1;
-   }
+    }
 
-   // find homography
+    // find homography
+    cv::Mat H;
+    H = cv::findHomography(pts1, pts2, cv::RANSAC);
 
-   cv::Mat H;
-   H = cv::findHomography(pts1, pts2, cv::RANSAC);
+    // warp perspective to stitch the images together
+    int nrows = images[0].rows;
+    int ncols = images[0].cols + images[1].cols;
+    cv::Mat result = cv::Mat(nrows, ncols, 0);
+    cv::warpPerspective(images[0], result, H, cv::Size(ncols,nrows));
+    
+    // set left half of result to images[1]
+    cv::Mat paddedImage;
+    // cv::hconcat(images[1], cv::Mat::zeros(images[0].rows, images[0].cols, 0), paddedImage);
+    /*
+    namedWindow("Padded", WINDOW_AUTOSIZE);
+    imshow("Padded", paddedImage);
+    waitKey(0);
 
-   // warp perspective to stitch the images together
-   cv::Mat result = cv::Mat(img1.rows, img1.cols + img2.cols, 0);
-   cv::warpPerspective(img1, result, H, cv::Size(img1.cols + img2.cols, img1.rows));
+    
+    printf("full rows: %d\n", full.rows);
+    printf("full cols: %d\n", full.cols);
+    printf("result rows: %d\n", result.rows);
+    printf("result cols: %d\n", result.cols);
+    */
+    result = result + paddedImage;
 
     namedWindow("Panorama", WINDOW_AUTOSIZE);
     imshow("Panorama", result);
     waitKey(0);
-   /*
-    // Draw good matches
-    cv::Mat output;
-    cv::drawMatches(images[0], allKeypoints[0], images[1], allKeypoints[1], goodMatches, output, Scalar::all(-1), Scalar::all(-1), std::vector< char >(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
+    cv::Mat matchesVis;
+    // cv::drawMatches(img1, kps1, img2, kps2, goodMatches, matchesVis);
+    /*
+    
     namedWindow("Display Matches", WINDOW_AUTOSIZE );
     imshow("Display Matches", output);
     waitKey(0); // Any keypress to exit
